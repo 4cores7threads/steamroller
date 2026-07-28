bcee7838bfbce54ad238ae157d9900b79078a24c
4cores7threads <dominicciccia68@gmail.com>
2026-07-28 15:27:39 -0400

    Update build.sh
    Would fail if bin directory was nonexistent

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
+if [ ! -d bin ]; then
+  mkdir bin
+fi
+
 g++ encryption.cpp legacy.cpp main.cpp -g -O3  -o bin/steamroller -I . -Llib -lm -Wl,-rpath,lib
-cd bin
-./steamroller benchmark
-cd ..
